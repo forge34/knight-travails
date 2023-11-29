fa7da94317a1f6ad32603972e1da63ec208e5e6f
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2023-11-29 23:27:46 +0200

    basic setup

---
+++ dist/app.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+let knightBoard = []
+
+console.log(knightBoard[0])
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBLDJCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1hcHAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsibGV0IGtuaWdodEJvYXJkID0gW11cblxuY29uc29sZS5sb2coa25pZ2h0Qm9hcmRbMF0pIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
